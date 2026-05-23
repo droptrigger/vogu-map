29a5f24766ab91c302208aa234c8c9ef881969a9
Vasily Domakov <vasya.domakov@mail.ru>
2026-05-23 23:03:17 +0300

    feat(migrations): добавить каскадное удаление для учебных корпусов

---
+++ src/VoguMap.Infrastructure/Persistence/Migrations/20260523194355_AddCascadeDeleteForRooms.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VoguMap.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCascadeDeleteForRooms : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "rooms_building_id_fkey",
+                table: "rooms");
+
+            migrationBuilder.AddForeignKey(
+                name: "rooms_building_id_fkey",
+                table: "rooms",
+                column: "building_id",
+                principalTable: "buildings",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "rooms_building_id_fkey",
+                table: "rooms");
+
+            migrationBuilder.AddForeignKey(
+                name: "rooms_building_id_fkey",
+                table: "rooms",
+                column: "building_id",
+                principalTable: "buildings",
+                principalColumn: "id",
+                onDelete: ReferentialAction.NoAction);
+        }
+    }
+}
